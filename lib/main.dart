26b3346e756add15d49287fbf8228eb7e6cdfd43
sirgb1 <sergeybolshakov@gmail.com>
2024-03-15 23:15:08 +0300

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application1/product_card.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CarList(),
+    );
+  }
+}
+
+class CarList extends StatelessWidget {
+  const CarList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Отечественный автопром', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.brown[200],
+      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(
+              10, (index) => carList[index]
+          )
+      ),
+    );
+  }
+}
